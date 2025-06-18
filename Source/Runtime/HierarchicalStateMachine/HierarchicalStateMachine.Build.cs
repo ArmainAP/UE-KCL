91f3589ac9ea849aaac647566baa88973445ffdb
ArmainAP <adrian.popoviciu@katcodelabs.com>
2025-06-18 18:05:42 +0300

    * Switched code optimization to shipping only.

--- Source/Runtime/HierarchicalStateMachine/HierarchicalStateMachine.Build.cs
+++ Source/Runtime/HierarchicalStateMachine/HierarchicalStateMachine.Build.cs
@@ -7,6 +7,7 @@ public class HierarchicalStateMachine : ModuleRules
 	public HierarchicalStateMachine(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		OptimizeCode = CodeOptimization.InShippingBuildsOnly;
 
 		PublicDependencyModuleNames.AddRange(new string[] 
 			{ 
