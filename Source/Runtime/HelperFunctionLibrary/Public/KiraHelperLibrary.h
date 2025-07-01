26c3740c6bc17aebd224a720ffef4415468b1425
ArmainAP <adrian.popoviciu@katcodelabs.com>
2025-07-01 16:19:07 +0300

    + Added template getters for Pawn and AIC

--- Source/Runtime/HelperFunctionLibrary/Public/KiraHelperLibrary.h
+++ Source/Runtime/HelperFunctionLibrary/Public/KiraHelperLibrary.h
@@ -16,9 +16,21 @@ class HELPERFUNCTIONLIBRARY_API UKiraHelperLibrary : public UBlueprintFunctionLi
 public:
 	UFUNCTION(BlueprintPure, Category = "KiraHelperLibrary")
 	static AAIController* GetAIController(AActor* Actor);
+
+	template<class T>
+	static T* GetAIController(AActor* Actor)
+	{
+		return Cast<T>(GetAIController(Actor));
+	}
 	
 	UFUNCTION(BlueprintPure, Category = "KiraHelperLibrary")
 	static APawn* GetPawn(AActor* Actor);
+
+	template<class T>
+	static T* GetPawn(AActor* Actor)
+	{
+		return Cast<T>(GetPawn(Actor));
+	}
 
 	UFUNCTION(BlueprintPure, Category = "KiraHelperLibrary")
 	static bool GetNavigablePathLenght(UWorld* WorldContextObject, const FVector& Start, const FVector& End, float& OutLength);
