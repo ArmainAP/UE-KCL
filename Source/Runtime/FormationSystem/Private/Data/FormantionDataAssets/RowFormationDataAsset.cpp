d5ba8f04c8bbfd7ba16c02eb95de7060b322f22f
ArmainAP <adrian.popoviciu@katcodelabs.com>
2025-08-28 21:02:39 +0300

    * Fixed index 0 improper coordinates

--- Source/Runtime/FormationSystem/Private/Data/FormantionDataAssets/RowFormationDataAsset.cpp
+++ Source/Runtime/FormationSystem/Private/Data/FormantionDataAssets/RowFormationDataAsset.cpp
@@ -4,10 +4,15 @@
 
 FTransform URowFormationDataAsset::GetOffsetTransformForIndex_Implementation(const int Index, const int UnitCount) const
 {
+	if (!Index)
+	{
+		return FTransform::Identity;
+	}
+	
 	const uint32 ColumnsCount = FMath::Min(MaxColumns, UnitCount);
 	
 	const float HalfSpan = (ColumnsCount - 1) * 0.5f;
-	
+		
 	const int32 RowIndex = Index / ColumnsCount;
 	const int32 InRowIndex = Index % ColumnsCount;
 	const int32 ColumnIndex = (ColumnsCount - 1) - InRowIndex;
