4a5e95fa203562cf98c92c80a8070e7c49dc38e4
ArmainAP <adrian.popoviciu@katcodelabs.com>
2025-09-02 12:59:10 +0300

    * Crash fixed caused by using lambda instead of weak lambda

--- Source/Runtime/ActivableObjects/Private/Components/DelayedActivationComponent.cpp
+++ Source/Runtime/ActivableObjects/Private/Components/DelayedActivationComponent.cpp
@@ -25,7 +25,7 @@ void UDelayedActivationComponent::TriggerDelayedActivation()
 	}
 	
 	GetWorld()->GetTimerManager().SetTimer(TimerHandle,
-		FTimerDelegate::CreateLambda([this]()
+		FTimerDelegate::CreateWeakLambda(this, [this]()
 		{
 			Activate();
 		}), ActivationDelay, false);
@@ -39,7 +39,7 @@ void UDelayedActivationComponent::TriggerDelayedDeactivation()
 	}
 	
 	GetWorld()->GetTimerManager().SetTimer(TimerHandle,
-	FTimerDelegate::CreateLambda([this]()
+	FTimerDelegate::CreateWeakLambda(this, [this]()
 	{
 		Deactivate();
 	}), ActivationDelay, false);
